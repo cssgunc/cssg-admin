0e0195fc51c238dd2c87ffb3444353416d252faa
pmoharana-cmd <pmoharana032474@gmail.com>
2026-02-15 06:03:14 +0000

    Add frontend containerization + docker compose

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,24 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+const NEXT_PUBLIC_PORT = Number(process.env.NEXT_PUBLIC_PORT) || 3000;
+
+app.use(
+  "/*",
+  cors({
+    origin: [`http://localhost:${NEXT_PUBLIC_PORT}`],
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
+
+app.get("/health", (c) => {
+  return c.json({
+    status: "ok",
+    message: "Backend is running",
+  });
 });
 
 const port = Number(process.env.PORT) || 8787;
--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,7 +1,22 @@
-import Image from "next/image";
+"use client";
+
 import test from "@cssg/shared";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState<string>("");
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:8787/health");
+      const data = await res.json();
+      console.log(data);
+      setData(data.message);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
@@ -16,6 +31,9 @@ export default function Home() {
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file. {test}
+          </h1>
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Data from backend: {data}
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
